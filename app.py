ba167d65a304d246412dbdbcadad03835e52a8cc
chris-kyle2 <22ms0010@iitism.ac.in>
2025-01-23 15:13:06 +0530

    Minor changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Hello, World!</title>
+            <title>Hello, World!100Ms</title>
             <style>
                 body {
                     display: flex;
